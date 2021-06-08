7f930f6d24e2fb861ce7ca38d39ce7bdcd7f69cf
acmolino <amolino.tester@gmail.com>
2021-06-08 10:07:04 -0300

    Update rutas.php

--- api/rutas.php
+++ api/rutas.php
@@ -1,7 +1,7 @@
 <?php
 defined('DS') ? null : define ('DS', DIRECTORY_SEPARATOR);
 
-defined('SITE_ROOT') ? null : define('SITE_ROOT', '');
+defined('SITE_ROOT') ? null : define('SITE_ROOT', 'https://cartasapi.vercel.app/api');
 
 defined('LOGICA') ? null : define('LOGICA', SITE_ROOT.DS.'logica'.DS);
 defined('CONFIG') ? null : define('CONFIG', SITE_ROOT.DS.'config'.DS);
