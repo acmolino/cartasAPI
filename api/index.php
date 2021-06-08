0474b2aefa07a790462a4963520bfc4196858da0
acmolino <amolino.tester@gmail.com>
2021-06-08 10:59:24 -0300

    Cambio

--- api/index.php
+++ api/index.php
@@ -1,18 +1,62 @@
 <?php
 header('Content-type: Application/JSON');
+//LAs rutas internas
 require_once "logica/mazo.php";
 require_once "logica/carta.php";
+//La clase para el routeo
+require_once 'config/router/autoload.php';
 
+//Inicio el enrutador
+$router = new AltoRouter();
 
-try {
+//Mi directorio base
+$router->setBasePath('/api');
+session_start();
+
+/**
+*
+*
+*
+*/
+$router->map('GET', '/iniciarMazo', function() {
+  try {
   	$_SESSION['mazoNuevo'] = new Mazo();
     $_SESSION['mazoNuevo']->armarMazo();
     http_response_code(200);
     echo '{"Message" : "Mazo iniciado con exito"}';
- } catch (\Exception $e) {
+  } catch (\Exception $e) {
     http_response_code(500);
     echo '{"Message" : "Error, no se pudo obtener mazo"}';
- }
+  }
+});
 
+/**
+*
+*
+*
+*/
+$router->map('GET', '/darCarta', function() { 
+  try {
+    echo json_encode($_SESSION['mazoNuevo']->mostrarDatos(), JSON_PRETTY_PRINT);
+    http_response_code(200);
+  } catch (\Exception $e) {
+    http_response_code(500);
+    echo '{"Message" : "Error al dar la carta"}';
+  }
+});
+
+
+/**
+*Necesario para el routeo
+*
+*
+*/
+$match = $router->match();
+if( is_array($match) && is_callable( $match['target'] ) ) {
+  call_user_func_array( $match['target'], $match['params'] );
+} else {
+  // no route was matched
+  header( $_SERVER["SERVER_PROTOCOL"] . ' 403 Not Found');
+}
 
 ?>
