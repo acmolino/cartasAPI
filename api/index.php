1a21a48e381c3dcde7039f453c8c414c6dd5abfc
acmolino <amolino.tester@gmail.com>
2021-06-08 12:16:09 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -15,8 +15,6 @@ $router = new AltoRouter();
 $router->setBasePath('/');
 session_start();
 
-$mazoPrueba = new Mazo();;
-
 /**
 *
 *
@@ -26,7 +24,6 @@ $router->map('GET', 'iniciarMazo', function() {
   try {
   	$_SESSION['mazoNuevo'] = new Mazo();
     $_SESSION['mazoNuevo']->armarMazo();
-    $mazoPrueba->armarMazo();
     http_response_code(200);
     echo '{"Message" : "Mazo iniciado con exito"}';
   } catch (\Exception $e) {
@@ -42,8 +39,7 @@ $router->map('GET', 'iniciarMazo', function() {
 */
 $router->map('GET', 'darCarta', function() { 
   try {
-    //echo json_encode($_SESSION['mazoNuevo']->mostrarDatos(), JSON_PRETTY_PRINT);
-    echo json_encode($mazoPrueba->mostrarDatos(), JSON_PRETTY_PRINT);
+    echo json_encode($_SESSION['mazoNuevo']->mostrarDatos(), JSON_PRETTY_PRINT);
     http_response_code(200);
   } catch (\Exception $e) {
     http_response_code(500);
